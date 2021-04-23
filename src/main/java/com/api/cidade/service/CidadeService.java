f8f3ba63e2cb5e17cebac43c6d4b7951cfc71060
mxdba <75141965+mxdba@users.noreply.github.com>
2021-04-23 20:10:52 -0300

    Projeto criado

--- src/main/java/com/api/cidade/service/CidadeService.java
+++ src/main/java/com/api/cidade/service/CidadeService.java
@@ -1,5 +1,20 @@
 package com.api.cidade.service;
 
-public class CidadeService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.api.cidade.model.CidadeModel;
+import com.api.cidade.repository.CidadeRepository;
+
+public class CidadeService implements ICidadeService {
+	
+	@Autowired
+	private CidadeRepository cidadeRepositoryy;
+	
+	
+	@Override
+	public List<CidadeModel> findAll() {
+		return (List<CidadeModel>) cidadeRepository.findAll();
+	}	
 }
